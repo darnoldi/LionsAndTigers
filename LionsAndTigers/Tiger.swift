085fa55c0e44de9ca4d260fcc42ea80fce006ffc
darnoldi <davearnoldi@Daves-iMac.local>
2014-09-16 11:18:03 +0200

    Lions and Tigers
    Phase 1

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,19 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Dave Arnoldi on 2014/09/15.
+//  Copyright (c) 2014 Dave Arnoldi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+    
+}
